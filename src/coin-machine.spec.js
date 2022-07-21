69e5a02fd7c62db63450590fda8ab7b44ddb748d
Magnus <magnushirst@gmail.com>
2022-07-21 16:49:37 +0100

    Adding test coverage for 20p coins

--- src/coin-machine.spec.js
+++ src/coin-machine.spec.js
@@ -4,26 +4,30 @@ describe("Coin Machine Tests", () => {
 
     // Tests that causes 100% mutation coverage
 
-    // test("it will return single pound coin", () => {
-    //     expect(coinMachine(100))
-    //         .toEqual("1x£1");
-    // });
-    // test("it will return single 2 pound coin", () => {
-    //     expect(coinMachine(200))
-    //         .toEqual("1x£2");
-    // });
-    // test("it will return multiple coins", () => {
-    //     expect(coinMachine(201))
-    //         .toEqual("1x£2 1x1p");
-    // });
-    // test("it will return multiple penny coins", () => {
-    //     expect(coinMachine(3))
-    //         .toEqual("1x2p 1x1p");
-    // });
-    // test("it will handle multiple coins from a decorated format", () => {
-    //     expect(coinMachine('£0.03'))
-    //         .toEqual("1x2p 1x1p");
-    // });
+    test("it will return single pound coin", () => {
+        expect(coinMachine(100))
+            .toEqual("1x£1");
+    });
+    test("it will return single 2 pound coin", () => {
+        expect(coinMachine(200))
+            .toEqual("1x£2");
+    });
+    test("it will return multiple coins", () => {
+        expect(coinMachine(201))
+            .toEqual("1x£2 1x1p");
+    });
+    test("it will return multiple penny coins", () => {
+        expect(coinMachine(3))
+            .toEqual("1x2p 1x1p");
+    });
+    test("it will handle 2 different coins from a decorated format", () => {
+        expect(coinMachine('£0.03'))
+            .toEqual("1x2p 1x1p");
+    });
+    test("it will handle all the different coins from a decorated format", () => {
+        expect(coinMachine('£3.23'))
+            .toEqual("1x£2 1x£1 1x20p 1x2p 1x1p");
+    });
 
     // Tests that causes 100% code coverage
 
